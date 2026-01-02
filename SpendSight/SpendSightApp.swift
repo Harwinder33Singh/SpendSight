a591e1ad9ddc18e8809d1acad3dc44861c62e09e
Harwinder Singh <hs612511@gmail.com>
2026-01-02 11:42:53 -0700

    Initial Commit

---
+++ SpendSight/SpendSightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpendSightApp.swift
+//  SpendSight
+//
+//  Created by Harwinder Singh on 1/2/26.
+//
+
+import SwiftUI
+
+@main
+struct SpendSightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
